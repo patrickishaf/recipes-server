83099915a7ba5e1af0d3cefe9779a619235722d4
oscarnovember <ndukwe.peter@roqqu.com>
2025-09-04 17:26:45 +0100

    allow requests from all origins

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import * as controller from './controller';
 
 const app = express()
+app.use(cors({ origin: true }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
